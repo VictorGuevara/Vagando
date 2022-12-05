4016edeca7e7c2468044b96914d6f50f7d2465cd
Ing. Víctor Guevara <VEMlone@hotmail.com>
2022-12-05 09:24:49 -0600

    Primer Commit

---
+++ js/login.js
@@ -0,0 +1,36 @@
+const container = document.querySelector('.container');
+const pwShowHide = document.querySelectorAll('.showHidePw');
+const pwFields = document.querySelectorAll('.password');
+const signup = document.querySelector('.signup-link');
+const login = document.querySelector('.login-link');
+
+// Funcion para cambiar la vista de la contaseña.
+pwShowHide.forEach(eyeIcon => {
+    eyeIcon.addEventListener('click', () => {
+        console.log('ok')
+        pwFields.forEach(pwField => {
+            if (pwField.type === 'password') {
+                pwField.type = 'text';
+
+                pwShowHide.forEach(icon => {
+                    icon.classList.replace('uil-eye-slash', 'uil-eye');
+                });
+            } else {
+                pwField.type = 'password';
+
+                pwShowHide.forEach(icon => {
+                    icon.classList.replace('uil-eye', 'uil-eye-slash');
+                });
+            }
+        });
+    });
+});
+
+// Eventos para aparecer y desaparecer los formularios.
+signup.addEventListener('click', () => {
+    container.classList.add('active');
+});
+
+login.addEventListener('click', () => {
+    container.classList.remove('active');
+});
